82f018129bbf5ec0d584bbf7849e56b701d2baef
wanchalong <nueng35@gmail.com>
2016-03-26 20:23:41 +0700

    bug3

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 5000))
+
 app.use(express.static('public'))
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!')
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
 })
